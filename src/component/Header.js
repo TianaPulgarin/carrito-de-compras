179d0aceb1bf45a6369c10a535ac8dd4cf268f75
TianaPulgarin <tianapulgarin@gmail.com>
2023-12-06 10:51:44 -0500

    I add general and header structure

---
+++ src/component/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/img/logo.png";
+
+function Header() {
+    return (
+        <header >
+            <div className="content-header">
+                <div className="content-logo">
+                    <img
+                        className="img-logo"
+                        src={Logo} alt="Logo" />
+                </div>
+                <p className="titulo-header"><Link to="/landing">Tienda</Link></p>
+                <p className="titulo-header"><Link to="/shopping">Carrito de compras</Link></p>
+
+            </div>
+        </header >
+    );
+
+
+}
+export default Header;
